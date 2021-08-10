cc504774c9d8167d24e13d6dd49e5b2f5217abb7
robertthome <remedy42290@gmail.com>
2021-08-10 12:01:39 -0700

    Relative path applied to images

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ const freePlayBtn = document.getElementById('free-play-btn')
 const timedGameBtn = document.getElementById('timed-game-btn')
 const instructionBtn = document.getElementById('instructions-btn')
 //fetched elements from gameboard
-const grid = document.querySelector('#gameGrid')
+
 const gameResetBtn = document.getElementById('game-reset-btn')
 
 let points = 0
-let cardsActive = []
-let gameBoard = []
 let cardFliped = false
 let gameActive = true
 
@@ -55,5 +53,14 @@ let cardList = [
   }
 ]
 
+//shuffles the imageis of the array
 cardList.sort(() => 0.5 - Math.random())
+const grid = document.querySelector('#gameGrid')
 console.log(cardList)
+// creates an img element for each image and appends it to the page
+cardList.forEach((eachImg) => {
+  let img = document.createElement('img')
+  img.src = eachImg.img
+  console.log(img)
+  console.log(eachImg)
+})
